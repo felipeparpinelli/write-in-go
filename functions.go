c0f103f9d2c45308cbdf63c579d204d8d6e553a2
Felipe Parpinelli <felipeparpinelli@gmail.com>
2017-01-22 18:39:55 -0200

    add another function definition

--- functions.go
+++ functions.go
@@ -12,6 +12,15 @@ func sum (n, m int) int {
 	return n + m
 }
 
+
+func PrecoFinal(precoCusto float64) (precoDolar float64, precoReal float64) {
+    fatorLucro := 1.33
+    taxaConversao := 2.34
+    precoDolar = precoCusto * fatorLucro
+    precoReal = precoDolar * taxaConversao
+	return
+}
+
 func main () {
 	printData("Fulano", 10)
 	s := sum(3, 5)
